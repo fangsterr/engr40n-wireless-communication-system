c2bbb9365f0da60b067fa38d84111b84433ed6d3
Andy Fang <andyfang@stanford.edu>
2013-04-15 00:15:39 -0700

    implement hamming

--- milestone1_starter/common_srcsink.py
+++ milestone1_starter/common_srcsink.py
@@ -9,4 +9,16 @@ def hamming(s1,s2):
     # the vector lengths) and then find the hamming distance
     # between the two. Also compute the bit error rate  .
     # BER = (# bits in error)/(# total bits )
-    return hamming_d, ber
+
+    # truncate if vectors are of different lengths
+    if len(s1) > len(s2):
+      s1 = s1[:len(s2)]
+    elif len(s2) > len(s1):
+      s2 = s2[:len(s1)]
+
+    num_error_bits = 0
+    for idx, val in enumerate(s1):
+      if val != s2[idx]:
+        num_error_bits = num_error_bits + 1
+
+    return num_error_bits, float(num_error_bits)/len(s1)
