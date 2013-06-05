b7aeb3bbd47744f618c879df8f8ab6cb72c529d1
Stanley Tang <account@stanleytang.com>
2013-06-05 11:33:38 -0700

    adsfsadf

--- milestone1_starter/common_txrx_mil3.py
+++ milestone1_starter/common_txrx_mil3.py
@@ -25,13 +25,13 @@ def demodulate(fc, samplerate, samples):
   demodulated_samples = []
   index = 0
   for sample in samples:
-    demodulated_sample = mod(fc, samplerate, index)
+    demodulated_sample = cmath.exp(2j * math.pi * fc * index / (samplerate * 1.0))
     demodulated_samples.append(demodulated_sample)
     index = index + 1
 
   omega_cut = 2 * math.pi * (fc / (2.0 * samplerate))
 
-  return lpfilter(demodulated_samples, omega_cut, fc, samplerate)
+  return lpfilter(demodulated_samples, omega_cut)
 
 def lpfilter(samples_in, omega_cut):
   '''
@@ -40,25 +40,36 @@ def lpfilter(samples_in, omega_cut):
   # set the filter unit sample response
   L = 50
 
-  # compute the demodulated samples
-  # This is probably wrong...
-  demod_samples = []
-  index = 0
-  for sample in samples_in:
+  unit_sample_response_array = []
+  index = L * (-1)
+  while index <= L:
     if index == 0:
       unit_sample_response = omega_cut / math.pi
-    elif (-1 * L) <= index <= L:
-      unit_sample_response = math.sin(omega_cut * index) / (math.pi * index * 1.0)
     else:
-      unit_sample_response = 1
+      unit_sample_response = math.sin(omega_cut * index) / (math.pi * index * 1.0)
+    unit_sample_response_array.append(unit_sample_response)
+    index = index + 1
+  unit_sample_response_array = numpy.array(unit_sample_response_array)
 
-    demod_sample = abs(sample * mod(fc, fs, index) * unit_sample_response)
-    demod_samples.append(demod_sample)
+  # compute the demodulated samples
+  demod_samples = []
+  index = 0
+
+  for sample in samples_in:
+    samples_subarray = numpy.zeros(len(unit_sample_response_array), dtype=numpy.dtype(complex))
+    samples_in_len = len(samples_in)
+    second_index = index + L
+    array_index = 0
+    while second_index >= index - L:
+      if second_index < 0 or second_index >= samples_in_len:
+        samples_subarray[array_index] = 0
+      else:
+        samples_subarray[array_index] = samples_in[second_index]
+      array_index = array_index + 1
+      second_index = second_index - 1
+
+    value = numpy.dot(unit_sample_response_array, samples_subarray)
+    demod_samples.append(abs(value))
     index = index + 1
 
   return demod_samples
-
-
-def mod(fc, fs, n):
-  return cmath.exp(2j * math.pi * fc * n / (fs * 1.0))
-
